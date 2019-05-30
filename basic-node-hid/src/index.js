137cc53f8a697cb696f64646cc65c974588be858
onlykey <t@T-2.local>
2019-05-29 21:18:52 -0400

    working settime example

--- basic-node-hid/src/index.js
+++ basic-node-hid/src/index.js
@@ -1,20 +1,58 @@
 import "babel-polyfill";
 import TransportNodeHid from "@trustcrypto/hw-transport-node-hid";
-import AppBtc from "@ledgerhq/hw-app-btc";
+import type Transport from "@trustcrypto/hw-transport";
+//import TransportWebAuthn from "@ledgerhq/hw-transport-webauthn";
+import HID from "node-hid";
 
-async function example() {
-  const transport = await TransportNodeHid.open("");
-  transport.setDebugMode(true);
-  const appBtc = new AppBtc(transport);
-  const result = await appBtc.getWalletPublicKey("44'/0'/0'/0/0");
-  return result;
+async function methodA() { //Try USB HID
+  const issupported = TransportNodeHid.isSupported();
+  if (issupported) { //Send settime and get response
+    TransportNodeHid.create().then(transport => {
+      let buffer = new Uint8Array(50);
+      buffer.fill(0, 0, 57);
+      buffer[0] = 0x5C;
+      buffer[1] = 0xEF;
+      buffer[2] = 0x0C;
+      buffer[3] = 0xB9; 
+      transport.send(0xE4, 0, 0, 0, buffer).then(response => {
+        //return transport.send(0xe0, 0x4a, 0xff, 0x00, buffer);
+      });
+    });
+  }
 }
 
-example().then(
+/*
+async function methodB()) { //If that doesn't work try Webauthn
+//TransportWebAuthn.create().then(transport => ...)
+}
+
+async function methodA() { //If that doesn't work try USB Keyboard
+}
+*/
+
+methodA().then( //Try USB HID
   result => {
     console.log(result);
   },
   e => {
     console.error(e);
+    /*
+    methodB().then( //If that doesn't work try Webauthn
+      result => {
+        console.log(result);
+      },
+      e => {
+        console.error(e);
+        methodC().then( //If that doesn't work try USB Keyboard
+          result => {
+            console.log(result);
+          },
+          e => {
+            console.error(e);
+          }
+        );
+      }
+    );
+    */
   }
 );
